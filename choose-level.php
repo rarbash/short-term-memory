42c0fbe6cc388644edc33abc8f99eb2b6a155ea2
Kawiya Pholjaroen <kawiyapholjaroen@Rabash.local>
2021-10-16 18:45:54 +0700

    push heroku

--- choose-level.php
+++ choose-level.php
@@ -1,40 +1,43 @@
 <html>
-<link rel="stylesheet" href="style.css">
-<div style="display: flex; margin-top:3em;">
-  <a href="/home.php">
-    <img src="icon/previous.png" alt="previous" class="iconsize-medium" style="margin-left:2em; position:absolute;">
-  </a>
-  <h1 style="justify-self: center; width:100%;">Please Select Level</h1>
-</div>
-<br><br><br>
-<div class="choose-btn-align">
-  <form action="user-name-input.php" action="question.php" method="post">
-  <!-- easy btn -->
-    <a href="/user-name-input.php">
-      <button class="btn-easy btn-easy-hover align-center" name="choose-level" value="1">
-        <img src="icon/easy.png" alt="medal" class="iconsize-small">
-        <strong class="margin-btn">Easy</strong>
-        <img src="icon/easy.png" alt="medal" class="iconsize-small">
-      </button>
+  <link rel="stylesheet" href="style.css">
+  <div style="display: flex; margin-top:3em;">
+    <a href="/home.php">
+      <img src="icon/previous.png" alt="previous" class="iconsize-medium" style="margin-left:2em; position:absolute;">
     </a>
-    <br><br><br>
-    <!-- medium btn -->
-    <a href="/user-name-input.php">
-      <button class="btn-medium btn-medium-hover align-center" name="choose-level" value="2">
-        <img src="icon/medium.png" alt="medal" class="iconsize-small">
-        <strong class="margin-btn">Medium</strong>
-        <img src="icon/medium.png" alt="medal" class="iconsize-small">
-      </button>
-    </a>
-    <br><br><br>
-    <!-- hard btn -->
-    <a href="/user-name-input.php">
-      <button class="btn-hard btn-hard-hover align-center" name="choose-level" value="3">
-        <img src="icon/hard.png" alt="medal" class="iconsize-small">
-        <strong class="margin-btn">Hard</strong>
-        <img src="icon/hard.png" alt="medal" class="iconsize-small">
-      </button>
-    </a>
-    </form>
-</div>
+    <h1 style="justify-self: center; width:100%;">Please Select Level</h1>
+  </div>
+  <br><br><br>
+  <div class="choose-btn-align">
+    <form action="user-name-input.php" action="question.php" method="post">
+    <!-- easy btn -->
+      <a href="/user-name-input.php" class="tooltip">
+        <button class="btn-easy align-center" name="choose-level" value="1">
+          <img src="icon/easy.png" alt="medal" class="iconsize-small">
+          <strong class="margin-btn">Easy</strong>
+          <img src="icon/easy.png" alt="medal" class="iconsize-small">
+        </button>
+        <span class="tooltiptext">easy mak mak</span>
+      </a>
+      <br><br><br>
+      <!-- medium btn -->
+      <a href="/user-name-input.php" class="tooltip">
+        <button class="btn-medium align-center" name="choose-level" value="2">
+          <img src="icon/medium.png" alt="medal" class="iconsize-small">
+          <strong class="margin-btn">Medium</strong>
+          <img src="icon/medium.png" alt="medal" class="iconsize-small">
+        </button>
+        <span class="tooltiptext">easy mak mak</span>
+      </a>
+      <br><br><br>
+      <!-- hard btn -->
+      <a href="/user-name-input.php" class="tooltip">
+        <button class="btn-hard align-center" name="choose-level" value="3">
+          <img src="icon/hard.png" alt="medal" class="iconsize-small">
+          <strong class="margin-btn">Hard</strong>
+          <img src="icon/hard.png" alt="medal" class="iconsize-small">
+        </button>
+        <span class="tooltiptext">easy mak mak</span>
+      </a>
+      </form>
+  </div>
 </html>
